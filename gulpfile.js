976a797a1979e9f2aee354ecf874518fe3a410ef
Yauhien Maciuk <YauhienMaciuk@sample.com>
2016-11-13 19:36:28 +0300

    2nd task

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,24 @@
 // Various helper modules
 var gulp = require("gulp");
-var plug = require("gulp-load-plugins")();
+var webserver = require('gulp-webserver');
+var ngAnnotate = require('gulp-ng-annotate');
 
-var source = [
-		'app/**/*.js', 
-		"vendor/**/*.*"
-	];
 
 gulp.task('webserver', function() {
-	return gulp
-		.src('app')										// root
-		.pipe(plug.webserver({
+	return gulp.src('app')								// root
+		.pipe(webserver({
 			livereload: true,							// livereload
 			directoryListing: true,
 			open: "http://localhost:8000/index.html"	// index.html
 	}));
 });
 
+
+gulp.task('ngAnnotate', function () {
+	return gulp.src(['app/**/*.js', "!app/vendor/**/*.*"])
+		.pipe(ngAnnotate())
+		.pipe(gulp.dest('dist'));
+});
 
 // The default task is 'webserver'
 gulp.task("default", ["webserver"]);
