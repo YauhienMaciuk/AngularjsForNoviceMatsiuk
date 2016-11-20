1709edbb570814a3dca2e8ad079757b0f762eae1
Yauhien Maciuk <YauhienMaciuk@sample.com>
2016-11-21 02:05:27 +0300

    4th task completed

--- app/feature/rank.service.js
+++ app/feature/rank.service.js
@@ -2,41 +2,61 @@
     "use strict";
 
     angular.module("feature")
-        .factory("rankSrv", rankSrv);
+        .provider("rankSrv", rankSrv);
 
-    function rankSrv() {
+    function rankSrv(defaultStarMsg) {
+        let message = defaultStarMsg;
         return {
-            rank
+            $get,
+            configRank
+        };
+
+        function $get() {
+            return {
+                rank
+            };
+
+            function rank(rankNumber, $ctrl) {
+                let star = "";
+                if (angular.isNumber(rankNumber)) {
+                    switch (true) {
+                        case (0 <= rankNumber && rankNumber < 101):
+                            star = $ctrl.oneStar;
+                            break;
+                        case (100 < rankNumber && rankNumber < 201):
+                            star = $ctrl.twoStar;
+                            break;
+                        case (200 < rankNumber && rankNumber < 501):
+                            star = $ctrl.threeStar;
+                            break;
+                        case (500 < rankNumber && rankNumber < 1001):
+                            star = $ctrl.fourStar;
+                            break;
+                        case (1000 < rankNumber):
+                            star = $ctrl.fiveStar;
+                            break;
+                        default:
+                            star = $ctrl.undefined;
+                            break;
+
+                    }
+                }
+                $ctrl.message = message;
+                $ctrl.star = star;
+                return star;
+            }
         }
 
-        function rank(rankNumber, $ctrl) {
-            let star = "";
-            if (angular.isNumber(rankNumber)) {
-                switch (true) {
-                    case (0 <= rankNumber && rankNumber < 101):
-                        star = $ctrl.oneStar;
-                        break;
-                    case (100 < rankNumber && rankNumber < 201):
-                        star = $ctrl.twoStar;
-                        break;
-                    case (200 < rankNumber && rankNumber < 501):
-                        star = $ctrl.threeStar;
-                        break;
-                    case (500 < rankNumber && rankNumber < 1001):
-                        star = $ctrl.fourStar;
-                        break;
-                    case (1000 < rankNumber):
-                        star = $ctrl.fiveStar;
-                        break;
-                    default:
-                        star = $ctrl.undefined;
-                        break;
-
-                }
+        function configRank(msgFromConfig) {
+            if(msgFromConfig) {
+                message = msgFromConfig;
+                return this;
             }
-            $ctrl.star = star;
-            return star;
+            else {
+                return message;
+            }
         }
     }
+
 
 })();
